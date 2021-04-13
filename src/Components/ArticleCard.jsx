a9c6e22622a75494b1b6c9aa43babed8f6623c92
Nadia <nadia.m.amir@gmail.com>
2021-04-13 22:23:32 +0100

    debug error on Card.Link

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,55 +1,28 @@
-import React from 'react';
+import React from "react";
 import { Link } from "@reach/router";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import { Votes } from './Votes';
-
-const useStyles = makeStyles({
-  root: {
-    // minWidth: 100,
-    // maxWidth: 400
-    'margin-left': '15%',
-    width: "70%",
-    height: "20%"
-  },
-  title: {
-    fontSize: 10,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
+import { Card } from "react-bootstrap";
+import { Votes } from "./Votes";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function ArticleCard(props) {
-  const classes = useStyles();
-  const {article} = props
+  const { article } = props;
 
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-        Date: {(new Date (article.created_at)).toDateString()}
-        </Typography>
-        <Typography variant="h6" component="h2">
+    <Card className="mx-auto" border='dark'  bg='light' style={{ width: "20rem" }}>
+      <Card.Body>
         <Link to={`/articles/${article.article_id}`}>
-        {article.title}
+          <Card.Title> {article.title}</Card.Title>
         </Link>
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-
-        </Typography>
-        <Typography variant="body2" component="p">
-        <Link to={`/articles/${article.article_id}`}>
-         {article.comment_count} comments
-         </Link>
-        </Typography>
-
-         <Votes votes={article.votes} id={article.article_id} endpoint="articles"/>
-   
-      </CardContent>
+        <Card.Subtitle className="mb-2 text-muted">
+          {" "}
+          posted in {article.topic} by {article.author}
+        </Card.Subtitle>
+        <Card.Link href={`/articles/${article.article_id}/comments`}>
+          {article.comment_count} comments
+        </Card.Link>
+        {/* <Card.Link href="#">Another Link</Card.Link> */}
+        <Votes votes={article.votes} id={article.article_id} endpoint="articles" />
+      </Card.Body>
     </Card>
   );
 }
-
